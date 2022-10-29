2238249df0780139bde5684f58977a525afab92a
Mihahanya <bs.mihon@gmail.com>
2022-10-29 23:04:04 +0300

    ...

--- build.js
+++ build.js
@@ -1,13 +1,13 @@
 
 function calculate_logical_expression(expr, vars) {
 	/*
-	The variable is a single latin letter, takes the value 0 or 1
+	The variable is a single latin varter, takes the value 0 or 1
 	The binary functions:
 	~ negation
 	* conjunction
 	+ disjunction
 	^ exclusive disjunction
-	-> material implication, <- converse implication
+	-> implication
 	= biconditional
 	*/
 	
@@ -16,55 +16,52 @@ function calculate_logical_expression(expr, vars) {
 	for (const var_name in vars) expr = expr.replaceAll(var_name, vars[var_name])
 	
 	// open the brackets by recursion
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '(') {
-			let from = ++i
-			let brc = 1
-			for (; brc != 0; i++) {
-				if (expr[i] == ')') brc--
-				else if (expr[i] == '(') brc++
-				
-				if (i > expr.length-1 && brc != 0) return null
+	
+	function find_brackets_content(str) {
+		// assumed that the first character is an opening bracket
+		for (var i=1; i<str.length; i++) {
+			for (var brc=1; brc != 0 && i<str.length; i++) {
+				if 		(str[i] == '(') brc++	
+				else if (str[i] == ')') brc--
 			}
-			brc_expr_res = calculate_logical_expression(expr.slice(from, i-1), vars)
-			if (brc_expr_res == null) return null
+			if (brc != 0) return null
+
+			return str.slice(1, i-1)
+		}
+	}
+
+	for (var i=0; i<expr.length; i++) {
+		if (expr[i] == '(') {
+			var brc_content = find_brackets_content(expr.substr(i)); 			if (brc_content == null) return null
+			var brc_expr_res = calculate_logical_expression(brc_content, vars);	if (brc_expr_res == null) return null
 			
-			expr = expr.slice(0, from-1) + brc_expr_res + expr.substr(i)
-			i = from-1
+			expr = expr.slice(0, i) + brc_expr_res + expr.substr(i+brc_content.length+2)
 		}
 		else if (expr[i] == ')') return null
 	}
 
-	for (let i=0; i<expr.length-1; i++) {
-		if (/^\d+$/.test(expr[i])) {
-			if (expr[i] > 1 || /^\d+$/.test(expr[i+1])) return null
+	// linear transformations
+	const operations = [
+		{ reg: /~[01]/, 		func: (_, a) => (a^1) }, 
+		{ reg: /[01]\*[01]/, 	func: (a, b) => (+a * +b) }, 
+		{ reg: /[01]\+[01]/, 	func: (a, b) => (Math.min(+a + +b, 1)) }, 
+		{ reg: /[01]\^[01]/, 	func: (a, b) => (a ^ b) }, 
+		{ reg: /[01]->[01]/, 	func: (a, b) => (+(a <= b)) }, 
+		{ reg: /[01]=[01]/, 	func: (a, b) => (+(a == b)) }, 
+	]
+
+	for (var op in operations) {
+		op = operations[op]
+		var match = expr.match(op.reg)
+		
+		while (match != null) {
+			expr = expr.replace(op.reg, op.func(match[0][0], match[0].slice(-1)))
+			match = expr.match(op.reg)
 		}
 	}
 
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '~') { expr = expr.slice(0, i).concat(expr[i+1]^1, expr.substr(i+2)); i-=1 }
-	}
-
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '*') { expr = expr.slice(0, i-1).concat(+expr[i-1]*+expr[i+1], expr.substr(i+2)); i-=1 }
-	}
-
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '+') { expr = expr.slice(0, i-1).concat(Math.min(+expr[i-1]+ +expr[i+1], 1), expr.substr(i+2)); i-=1 }
-	}
-
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '^') { expr = expr.slice(0, i-1).concat(+expr[i-1]^+expr[i+1], expr.substr(i+2)); i-=1 }
-	}
-
-	for (let i=0; i<expr.length-1; i++) {
-		if (expr.substr(i, 2) == '<-') { expr = expr.slice(0, i-1).concat(expr[i-1] >= expr[i+2], expr.substr(i+3)); i-=2 }
-		if (expr.substr(i, 2) == '->') { expr = expr.slice(0, i-1).concat(expr[i-1] <= expr[i+2], expr.substr(i+3)); i-=2 }
-	}
-
-	for (let i=0; i<expr.length; i++) {
-		if (expr[i] == '=') { expr = expr.slice(0, i-1).concat(expr[i-1] == expr[i+1], expr.substr(i+2)); i-=1 }
-	}
+	// some syntax error
+	if (expr.length > 1 || !/\d/.test(expr)) return null
 
 	return expr
 }
